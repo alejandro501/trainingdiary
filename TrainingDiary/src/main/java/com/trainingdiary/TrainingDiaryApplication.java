ecc1047ee12f15b7b774654c907b6538cea205bd
Alexander Soltész <alexander.soltesz1@gmail.com>
2018-09-14 18:55:47 +0200

    Initial commit

---
+++ TrainingDiary/src/main/java/com/trainingdiary/TrainingDiaryApplication.java
@@ -0,0 +1,12 @@
+package com.trainingdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingDiaryApplication.class, args);
+	}
+}
---
+++ TrainingDiary/src/test/java/com/trainingdiary/TrainingDiaryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.trainingdiary;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TrainingDiaryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
